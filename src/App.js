168f7dbb1b80c8ade31fa8ed826178682eb957d8
Husna <ucarisikhusna@gmail.com>
2020-12-19 19:17:38 +0300

    Adding Header Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router ,Switch , Route} from "react-router-dom";
-
+import Header from "./header/Header"
 
 function App() {
   return (
     <Router>
-    <div className="App">
-    <Switch>
-
-     <Route path="/login">Login</Route>
-     <Route path="/login">Logout</Route>
-     <Route path="/header">Header</Route>
-    </Switch>
-    </div>
+      <div className="App">
+        <Header />
+        <Switch>
+         <Route path="/login">Login</Route>
+         <Route path="/register">Register</Route>
+         <Route path="/"></Route>
+        </Switch>
+     </div>
     </Router>
   );
 }
